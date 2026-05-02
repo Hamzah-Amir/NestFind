7645d972fcc0486909d1f25ad909197c01f64b32
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-02 15:20:21 +0500

    extracted all data from url

--- listings/views.py
+++ listings/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def listings(request):
+    if request.method == "GET":
+        min_price = request.GET.get('min_price', 0)
+        max_price = request.GET.get('max_price', 0)
+        location = request.GET.get('location', '')
+        property_type = request.GET.get('property_type', '')
     return HttpResponse("<h1>This is listing page</h1>")
