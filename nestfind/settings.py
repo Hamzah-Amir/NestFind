f0d4a6f273fa03f8de30049f049429b24ccb808f
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-14 22:56:18 +0500

    added accounts app

--- nestfind/settings.py
+++ nestfind/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django_browser_reload',
     'core.apps.CoreConfig',
     'listings',
-    'pages'
+    'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -88,8 +89,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -109,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = ''
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
