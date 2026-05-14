257942dd0f7225f0e189aa25042b083cf2dbb6e2
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-14 22:57:21 +0500

    added accounts app

--- nestfind/settings.py
+++ nestfind/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = ''
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
