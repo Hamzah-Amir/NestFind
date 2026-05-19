4e3be54efc98d9a1f3037f178f1d70c4a6c2823e
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-19 10:31:36 +0500

    created basic view for listings

--- listings/views.py
+++ listings/views.py
@@ -21,4 +21,5 @@ def listings(request):
             filter['property_type'] = property_type
         # Filtering listings based on the provided criteria
         listings = Listing.objects.filter(**filter)
+        print("title",listings[0].title)
         return render(request, "listings/listing.html", {'listings': listings})
