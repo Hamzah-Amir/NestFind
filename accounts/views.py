4581b6ea78c0058e55f64871c017544dddb784bf
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-25 23:43:09 +0500

    login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
+        print(f"Attempting to authenticate user with email: {email}")
+        if user is not None:
+            login(request, user)
+            print(f"User {email} authenticated successfully.")
+            return redirect("core:home")
+        else:
+            print(f"Failed to authenticate user with email: {email}")
+            return render(request, "accounts/login.html", {"error": "Invalid email or password."})
+        
     return render(request, "accounts/login.html")
 
 def register_view(request):
