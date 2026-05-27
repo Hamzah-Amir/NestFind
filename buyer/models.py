4993c24cb6a196c644e1cc34ce1eff145a9ccaff
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-05-27 11:40:29 +0500

    created favorite model

--- buyer/models.py
+++ buyer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Favorite(models.Model):
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='favorites')
+    listing = models.ForeignKey('listings.Listing', on_delete=models.CASCADE, related_name='favorited_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'listing')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.listing.title}"
