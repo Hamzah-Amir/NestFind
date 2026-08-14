9825d7cce0f2371c28c553b09a2586c55d9df6f5
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-08-14 23:23:12 +0500

    migration for listingview model

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Listing, ListingImage
+from .models import Listing, ListingImage, ListingView
 
 # Register your models here.
 admin.site.register(Listing)
+admin.site.register(ListingView)
 admin.site.register(ListingImage)
