c3d32277274a1817efa8e03bdbbd07f612986bd3
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-08-15 14:34:34 +0500

    created all dashboard urls

--- seller/urls.py
+++ seller/urls.py
@@ -5,5 +5,9 @@ app_name = 'seller'
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/listing/add/', views.create_listings, name='create_listing'),
+    path('listing/add/', views.create_listings, name='create_listing'),
+    path('listings/', views.my_listings, name='listings'),
+    path('listings/<slug:slug>/delete/', views.delete_listings, name='delete_listings'),
+    path('inquiry/', views.inquiry, name='inquiry'),
+    path('setting/', views.settings, name='settings'),
 ]
